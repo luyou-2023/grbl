239f3d96d10b4c5f7cc0bbfea52c300d20a23ad3
MillerRen <ren12345@126.com>
2022-01-08 17:24:56 +0800

    。

--- grbl/main.c
+++ grbl/main.c
@@ -5,11 +5,11 @@
   版权所有 2011-2016 Sungeun K. Jeon for Gnea Research LLC
   版权所有 2009-2011 Simen Svale Skogsrud
   
-  Grbl 是自由软件：你可以在自由软件基金会的GNU 普通公共许可(GPL v3+)条款下发行，或修改它.
+  Grbl 是自由软件：你可以在自由软件基金会的GNU 普通公共许可(GPL v3+)条款下发行，或修改它。
   Grbl的发布是希望它能有用，但没有任何保证;甚至没有隐含的保证适销性或适合某一特定目的。
   更多详细信息，请参阅GNU通用公共许可证。
 
-  您应该已经收到GNU通用公共许可证的副本和Grbl一起。如果没有，请参阅<http://www.gnu.org/licenses/>.
+  您应该已经收到GNU通用公共许可证的副本和Grbl一起。如果没有，请参阅<http://www.gnu.org/licenses/>。
 */
 
 #include "grbl.h"
